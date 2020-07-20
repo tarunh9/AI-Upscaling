75b197ea003009bdfe37a5a31fcd1d6328442ed8
Tarun Venkatesh H <tarunvenkatesh.cs17@bmsce.ac.in>
2020-07-20 20:23:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 from PIL import Image
 
 
@@ -13,7 +12,6 @@ def plot_sample(lr, sr):
 
     images = [lr, sr]
     titles = [f'Original Image', f'AI Upscaled Image']
-    # titles = ['.', '.']
     for i, (img, title) in enumerate(zip(images, titles)):
         plt.subplot(1, 2, i+1)
         plt.imshow(img)
